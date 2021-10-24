06828c61d5b075de1859b320e989e556a01e6a76
ElenaStepanchuk <podolyak365@gmail.com>
2021-10-24 23:16:08 +0300

    task 1 start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galeryElementsRefs = document.querySelector('.gallery');
+ 
+galeryElementsRefs.insertAdjacentHTML('beforeend', createGalleryItems);
+
+function createGalleryItems(gallery) {
+  return gallery
+    .map(({ element, index }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${element.original}">
+    <img class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      data-index="${index}"
+      alt="${element.description}"/>
+  </a>
+</div>`;
+    })
+    .join('');   
+}
+
+    
+
+
