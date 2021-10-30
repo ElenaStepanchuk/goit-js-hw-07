56afec89a8144289449d1f77451a7a60a1baeb99
ElenaStepanchuk <podolyak365@gmail.com>
2021-10-30 11:14:09 +0300

    task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,62 +1,93 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
+
+let currentIndex = 0;
 
 const galeryElementsRefs = document.querySelector('.gallery');
  
 
 const createGalleryItems = galleryItems
-    .map(({ preview, original, description  }) => {
+    .map(({ preview, original, description  }, index) => {
     return `<div class="gallery__item">
     <a class="gallery__link" href="${original}" data-lightbox="roadtrip">
     <img class="gallery__image"
       src="${preview}"
       data-source="${original}"
+      data-index="${index}"
       alt="${description}"/>
   </a>
 </div>`;
     })
     .join('');   
-console.log(createGalleryItems);
 
 galeryElementsRefs.insertAdjacentHTML('beforeend', createGalleryItems);
     
 galeryElementsRefs.addEventListener('click', hendleGalleryClick);
 
-let instance = '';
+const options = {
+  onShow: () => {
+    window.addEventListener('keydown', handleKeydown);
+        
+  },
+  onClose: () => {
+    window.removeEventListener('keydown', handleKeydown);
+        
+  },
+};
 
-function hendleGalleryClick(event) {
-  if (!event.target.classList.contains('gallery__item')) {
-    return;
-  }
-  event.preventDefault();
-  const getUrlImage = event.target.dataset.source;
-  console.log(getUrlImage);
-
-  
-  instance = basicLightbox.create(`
+const instance = basicLightbox.create(`
     <div class="modal">
        <img width="1400" height="900"
-       src="${imageSource}">
-    </div>`,
-    {
-      onShow: instance => {
-        window.addEventListener('keydown', onEscClick);
-      },
-    },
-    {
-      onClose: instance => {
-        window.removeEventListener('keydown', onEscClick);
-      },
-    },
-  );
+       src="">
+    </div>`, options);
 
-  instance.onShow();
+function hendleGalleryClick(event) {
+  event.preventDefault();
+ 
 
-  function escKeyPress(event) {
-    if (event.code === 'Escape') {
-      modalWindow.close();
-      instance.onClose();
-    }
-  }
+  if (event.target.nodeName !== 'IMG') return;
+ 
+  // const getUrlImage = event.target.dataset.source;
+  // console.log(getUrlImage);
+  setOriginalUrl(event.target.dataset.source);
+  currentIndex = Number(event.target.dataset.index);
+  
+  instance.show();
+
+  
 }
+
+function handleKeydown({key}) {
+  switch (key) {
+    case 'ArrowRight':
+      currentIndex += 1;
+    
+      if (currentIndex >= galleryItems.length) {
+        currentIndex = 0;
+      }
+    
+      setOriginalUrl(galleryItems[currentIndex].original);
+      break;
+    case 'ArrowLeft':
+      currentIndex -= 1;
+    
+      if (currentIndex <= 0) {
+        currentIndex = galleryItems.length - 1;
+      }
+    
+      setOriginalUrl(galleryItems[currentIndex].original);
+      break;
+    
+    case 'Escape':
+      instance.close();
+      break;
+    
+    default:
+      alert('Что-то пошло не так!');
+  }
+    
+}
+function setOriginalUrl(url) {
+  instance.element().querySelector("img").src = url;
+  return;
+  }
